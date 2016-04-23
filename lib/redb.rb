25cd91424aba0ec29466f36e029ba21a84c2a957
selfup <boudinot.regis@yahoo.com>
2016-04-23 17:38:32 +0000

    using constant for lambda - pushed logic back into new_data

--- lib/redb.rb
+++ lib/redb.rb
@@ -1,23 +1,18 @@
 require 'open-uri'
 
 class ReDb
-  attr_reader :open_and_write, :read_and_append
 
   def initialize
     return Dir.mkdir("redb") if !File.exist?("redb")
-    @open_and_write = ->(method, table_name) do
-      return if File.exist?(table_name)
-      File.open("./redb/#{table_name}", "w+"){|x| x.write(method)}
-    end
-    @read_and_append = -> (table_name, data) do
-      table = read_table(table_name)
-      table["#{table['0']['nextId'] + 1}"] = data
-      File.open("./redb/#{table_name}", "w+"){|x| x.write(table)}
-    end
+  end
+
+  OPEN_AND_WRITE = ->(method, table_name) do
+    return if File.exist?(table_name)
+    File.open("./redb/#{table_name}", "w+"){|x| x.write(method)}
   end
 
   def create_table(table_name)
-    open_and_write.(meta_data(table_name), table_name)
+    OPEN_AND_WRITE.(meta_data(table_name), table_name)
   end
 
   def read_table(table_name)
@@ -29,7 +24,9 @@ class ReDb
   end
 
   def new_data(table_name, data)
-    read_and_append.(table_name, data)
+    table = read_table(table_name)
+    table["#{table['0']['nextId'] + 1}"] = data
+    File.open("./redb/#{table_name}", "w+"){|x| x.write(table)}
   end
 
   private
